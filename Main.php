739721e9e3aa3011522e234a6fe5da173dd20c08
ItzWalla <artzywalla@gmail.com>
2017-03-25 01:39:58 -0700

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,22 +1,32 @@
 <?php
 
-
-namespace lol;
+namespace wallito;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\PlayerJoinEvent;
-use pocketmine\utils\TextFormat;
-use pocketmine\event\Listener;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Player;
+use pocketmine\Server;
+
+use pocketmine\utils\TextFormat as TF;
+
+class Main extends PluginBase {
+
+  public function onEnable (){
+    $this->getLogger()->info(TF::GREEN . ("Wallito's first plugin has been enabled!"))
 
 
-class Main extends implements Listener
-{
-public function onEnable(){
-$this->getServer()->getPluginManager()->registerEvents($this, $this);
-}
-public function onJoin(PlayerJoinEvent $e) 
-{
-$p = $e->getPlayer();
-       $p->sendMessage("Hi! Welcome to wallas porn site! Here you will find turtle porn!");
-    }
+
+   public function onCommand(CommandSender $player, Command $command, $label, array $args){
+    if(strtolower($command->getName()) === "test"){
+      if($player->hasPermission("wallito.cmd")) {
+         $player->sendMessage("§cYou have been healed by the great wallito!")
+         $player->setHealt(20.0);
+} elseif(!$player->hasPermission("wallito.cmd");
+    $player->sendMessage("Hey dumbass you dont have perms!");
+     }
+   }
+  }
+   public function onDisable() {
+     $this->getLogger()->info(TF::RED . ("Shit!);
 }
